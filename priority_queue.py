8a661d4004cc16b1d31c30ff8998b18c6b483c8e
szmisztal <sz.misztal@gmail.com>
2024-03-03 13:37:43 +0100

    add Queue and Element classes, add first methods for Queue

--- priority_queue.py
+++ priority_queue.py
@@ -1,1 +1,29 @@
+import datetime
 
+
+class Queue:
+    def __init__(self):
+        self.elements_list = []
+    def add_item(self):
+        pass
+
+    def get_first_item_and_delete_it(self):
+        pass
+
+    def get_one_item(self, index):
+        pass
+
+    def check_list_is_empty(self):
+        if len(self.elements_list) == 0:
+            return True
+        else:
+            return False
+
+    def count_items(self):
+        return len(self.elements_list)
+
+
+class Element:
+    def __init__(self, priority):
+        self.priority = priority
+        self.appearance_time = datetime.datetime.now()

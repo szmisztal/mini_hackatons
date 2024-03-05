56a0553049c08d0b92f9c08659194af14d00b1b5
szmisztal <sz.misztal@gmail.com>
2024-03-05 14:33:31 +0100

    now get_item_with_highest_priority_and_delete_it() method returns oldest element with highest priority, add some test code

--- priority_queue.py
+++ priority_queue.py
@@ -1,5 +1,5 @@
 import datetime
-
+import random
 
 class Queue:
     def __init__(self):
@@ -9,14 +9,35 @@ class Queue:
         self.elements_list.append(item)
 
     def get_item_with_highest_priority_and_delete_it(self):
-        sorted_elements_list_by_priority = sorted(self.elements_list, key = lambda x: x.priority, reverse = True)
-        return sorted_elements_list_by_priority.pop(0)
+        check_list_is_empty = self.check_list_is_empty()
+        if check_list_is_empty == False:
+            sorted_elements_list_by_priority = sorted(self.elements_list, key = lambda x: x.priority, reverse = True)
+            elements_with_highest_priority = []
+            highest_priority = sorted_elements_list_by_priority[0].priority
+            for element in sorted_elements_list_by_priority:
+                if element.priority == highest_priority:
+                    elements_with_highest_priority.append(element)
+                else:
+                    break
+            sorted_elements_with_highest_priority_by_appearance_time = sorted(elements_with_highest_priority, key = lambda x: x.appearance_time)
+            expected_element = sorted_elements_with_highest_priority_by_appearance_time[0]
+            expected_element_index = self.elements_list.index(expected_element)
+            return self.elements_list.pop(expected_element_index)
+        else:
+            return "List is empty"
 
     def get_one_item(self, index):
-        return self.elements_list[index]
+        try:
+            check_list_is_empty = self.check_list_is_empty()
+            if check_list_is_empty == False:
+                return self.elements_list[index]
+            else:
+                return "List is empty"
+        except IndexError as e:
+            return e
 
     def check_list_is_empty(self):
-        if len(self.elements_list) == 0:
+        if self.count_items() == 0:
             return True
         else:
             return False
@@ -26,19 +47,30 @@ class Queue:
 
 
 class Element:
-    def __init__(self, priority):
+    def __init__(self, id, priority):
+        self.id = id
         self.priority = int(priority)
         self.appearance_time = datetime.datetime.now()
 
     def __repr__(self):
-        return f"Element with {self.priority}-priority"
+        return f"Element_{self.id} with {self.priority}-priority, element appearance time = {self.appearance_time}"
 
-
+# for testing
 queue = Queue()
-one = Element(1)
-two = Element(2)
-three = Element(3)
-queue.add_item(one)
-queue.add_item(two)
-queue.add_item(three)
+obj_1 = Element(len(queue.elements_list) + 1, random.randint(1, 10))
+queue.add_item(obj_1)
+obj_2 = Element(len(queue.elements_list) + 1, random.randint(1, 10))
+queue.add_item(obj_2)
+obj_3 = Element(len(queue.elements_list) + 1, random.randint(1, 10))
+queue.add_item(obj_3)
+obj_4 = Element(len(queue.elements_list)+ 1, random.randint(1, 10))
+queue.add_item(obj_4)
+obj_5 = Element(len(queue.elements_list) + 1, random.randint(1, 10))
+queue.add_item(obj_5)
+print(queue.elements_list)
+print(queue.count_items())
+print(queue.get_one_item(4))
+print(queue.get_one_item(10))
 print(queue.get_item_with_highest_priority_and_delete_it())
+print(queue.count_items())
+
